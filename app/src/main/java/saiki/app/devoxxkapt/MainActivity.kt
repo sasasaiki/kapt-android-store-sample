c2f9925610fee9239c72360c9910dec2d2af5e31
saiki-iijima <saiiijim@yahoo-corp.jp>
2018-11-19 20:49:42 +0900

    改行追加

--- app/src/main/java/saiki/app/devoxxkapt/MainActivity.kt
+++ app/src/main/java/saiki/app/devoxxkapt/MainActivity.kt
@@ -26,6 +26,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
 
 }
